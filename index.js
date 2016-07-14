bc8bc3a81922ae471ceaaa86e97d2d98da012a99
pogopaule <pogopaule@gmx.de>
2016-07-14 09:48:53 +0200

    add missing semicolon

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 
 module.exports = {
   name: 'ember-cli-qunit-runtime',
+
   included: function(app) {
     app.import('vendor/ember-cli-qunit-runtime/qunit-configuration.js', {
       type: 'test'
-    })
+    });
   }
 };
