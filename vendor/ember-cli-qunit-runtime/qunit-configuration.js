38a7eacf8776ae0f7bb3aa5af4d9e25cb9e81e44
pogopaule <pogopaule@gmx.de>
2016-07-14 09:49:22 +0200

    fix qunit-configuration
    if the code runs before the document is ready, QUnit cannot be found

--- vendor/ember-cli-qunit-runtime/qunit-configuration.js
+++ vendor/ember-cli-qunit-runtime/qunit-configuration.js
@@ -1,20 +1,22 @@
-/* globals QUnit */
+/* globals $,QUnit */
 
-QUnit.config.urlConfig.push({ id: 'sortruntime', label: 'Sort by runtime'});
+$(document).ready(function() {
+  QUnit.config.urlConfig.push({ id: 'sortruntime', label: 'Sort by runtime'});
 
-QUnit.done(function(details) {
-  if(QUnit.urlParams.sortruntime) {
-    var extractRuntime = function(listItem) {
-      return parseInt($($(listItem).find('.runtime')[0]).text().slice(0, -3))
-    };
+  QUnit.done(function() {
+    if(QUnit.urlParams.sortruntime) {
+      var extractRuntime = function(listItem) {
+        return parseInt($($(listItem).find('.runtime')[0]).text().slice(0, -3));
+      };
 
-    var sortTests = function(a, b) {
-      return extractRuntime(b) - extractRuntime(a);
-    };
+      var sortTests = function(a, b) {
+        return extractRuntime(b) - extractRuntime(a);
+      };
 
-    var activeTests = $('#qunit-tests > .pass, #qunit-tests > .fail');
-    var sortedListItems = activeTests.detach().sort(sortTests);
+      var activeTests = $('#qunit-tests > .pass, #qunit-tests > .fail');
+      var sortedListItems = activeTests.detach().sort(sortTests);
 
-    $('#qunit-tests').prepend(sortedListItems);
-  }
+      $('#qunit-tests').prepend(sortedListItems);
+    }
+  });
 });
